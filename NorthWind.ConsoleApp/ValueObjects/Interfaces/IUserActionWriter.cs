db4e7f74dafa65f8722b4e3c8c74196d45eb693e
melissa <melissavallecilla0821@gmail.com>
2024-10-14 16:06:35 -0500

    Nuevo Proyecto

---
+++ NorthWind.ConsoleApp/ValueObjects/Interfaces/IUserActionWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.ValueObjects.Interfaces
+{
+    public interface IUserActionWriter
+
+    {
+        void Write(UserAction action);
+    }
+}
--- NorthWind.ConsoleApp/ValueObjects/UserActions.cs
+++ NorthWind.ConsoleApp/ValueObjects/UserActions.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NorthWind.Entities.ValueObjects
+namespace NorthWind.Entities.ValueObjects;
+public class UserAction(string user, string description)
 {
     internal class UserActions
     {
+        public DateTime CreatedDateTime { get; } = DateTime.Now;
+        public string User => User;
+        public string Description => Description;
+
     }
 }
