4f81de3a9bedb2fcd65a75fdb314fc06114db13e
melissa <melissavallecilla0821@gmail.com>
2024-10-14 18:39:00 -0500

    Nuevo Proyecto

--- Norwind.ConsoleAPP/Services/AppLoger.cs
+++ Norwind.ConsoleAPP/Services/AppLoger.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NorthWind.Entities.ValueObjects;
+using NorthWind.Entities.ValueObjects.Interfaces;
 
 namespace Norwind.ConsoleAPP.Services
 {
-    internal class AppLoger
+    internal class AppLoger(IUserActionWriter writer)
     {
-        public void Writelog(string message) { }
+        public void Writelog(string message)
+        {
+            UserAction Log = new UserAction("System", message);
+            writer.Write(Log);
+        }
     }
 }
