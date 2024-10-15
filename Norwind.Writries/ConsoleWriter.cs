62a4a66539608b03c5840be827fbb4d7585aee9f
melissa <melissavallecilla0821@gmail.com>
2024-10-15 10:16:48 -0500

    Agregue Interfaz

--- Norwind.Writries/ConsoleWriter.cs
+++ Norwind.Writries/ConsoleWriter.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Norwind.Writries
 {
-    internal class ConsoleWriter
+    internal class ConsoleWriter : IUserActionWriter
+    {
+    }
+
+   public  interface IUserActionWriter
     {
     }
 }
